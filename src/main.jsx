a30c35097a277226d9fadf805f8a97875e476baf
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-03-14 00:49:00 +0530

    Changes Applied

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { shadesOfPurple } from "@clerk/themes";
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
